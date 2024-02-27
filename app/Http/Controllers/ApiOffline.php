a7d61012db357be81c40962ebdf917da84dc09f6
Servidor test <Servidor Test>
2024-02-27 17:09:37 +0000

    COmpleto

---
+++ app/Http/Controllers/ApiOffline.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Induction;
+use App\Models\InductionWorker; // Asegúrate de importar el modelo InductionWorker
+
+use Carbon\Carbon;
+use DateTimeZone; // Importa la clase DateTimeZone
+
+class ApiOffline extends Controller
+{
+    public function DownloadData(Request $request)
+    {
+        try {
+            $id_company = $request->input('id_company');
+
+            $currentDateTime = Carbon::now(new DateTimeZone('America/Lima'));
+
+            $inductions = Induction::where('id_company', $id_company)
+                ->whereRaw("CONCAT(date_end, ' ', time_end) >= ?", [$currentDateTime->toDateTimeString()])
+                ->orderBy('id', 'desc')
+                ->get();
+
+            $inductionsArray = [];
+
+            foreach ($inductions as $induction) {
+                $induction_workers = InductionWorker::where('id_induction', $induction->id)->get();
+
+                foreach ($induction_workers as $induction_worker) {
+                    if ($induction_worker->num_report < $induction->intentos) {
+                        $inductionsArray[] = [
+                            'dni' => $induction_worker->worker->user->doi,
+                            'intento' => $induction_worker->num_report + 1,
+                            'cabecera_id' => $induction_worker->id,
+                            'induction' => [
+                                'induction_id' => $induction->id,
+                                'id_workshop' => $induction->id_workshop,
+                                'cabecera_id' => $induction_worker->id,
+                                'intento' => $induction_worker->num_report + 1,
+                                'intentos' => $induction->intentos,
+                                'fecha_inicio' => $induction->date_start . ' ' . $induction->time_start,
+                                'fecha_fin' => $induction->date_end . ' ' . $induction->time_end,
+                                'taller' => $induction->alias,
+                                'nombre' => $induction_worker->worker->nombre,
+                                'apellido' => $induction_worker->worker->apellido,
+                                'cargo' => $induction_worker->worker->position,
+                                'celular' => $induction_worker->worker->celular,
+                                'nombre_servicio' => $induction_worker->worker->service->name,
+                                'id_service' => $induction_worker->worker->id_service,
+                            ]
+                        ];
+                    }
+                }
+            }
+
+            return response()->json([
+                'success' => true,
+                'data' => $inductionsArray
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage()
+            ]);
+        }
+    }
+}
---
+++ public/apiunity/insertNotacopy.php
@@ -0,0 +1,149 @@
+<?php
+require("conexion.php");
+
+// Recibir los datos del JSON y otros parámetros
+$cabecera_id = $_POST['cabecera_id'];
+$nuevoIntento = $_POST['intento'];
+$note = $_POST['note'];
+$note_reference = $_POST['note_reference'];
+$start_date = $_POST['start_date'];
+$end_date = $_POST['end_date'];
+$rol = $_POST['rol'];
+$jsonData = $_POST['json']; // Esto ya contiene el JSON decodificado
+$entrenamiento = intval($_POST['entrenamiento']);
+
+// Imprimir los valores recibidos
+// echo "cabecera_id: " . $cabecera_id . "<br>";
+// echo "nuevoIntento: " . $nuevoIntento . "<br>";
+// echo "note: " . $note . "<br>";
+// echo "note_reference: " . $note_reference . "<br>";
+// echo "start_date: " . $start_date . "<br>";
+// echo "end_date: " . $end_date . "<br>";
+// echo "rol: " . $rol . "<br>";
+// echo "jsonData: " . $jsonData . "<br>";
+// echo "entrenamiento: " . $entrenamiento . "<br>";
+
+$intentoEntrenamiento = 0;
+if ($entrenamiento == 1) {
+  $stmt = $db->prepare("SELECT COALESCE(MAX(report) + 1, 1) AS result FROM detail_induction_workers WHERE induction_worker_id = :id AND entrenamiento = '1'");
+  $stmt->bindParam(':id', $cabecera_id);
+  $stmt->execute();
+  $result = $stmt->fetch(PDO::FETCH_ASSOC);
+  $intentoEntrenamiento = $result['result'];
+}
+
+// echo $entrenamiento;
+
+$jsonData = json_decode($jsonData, true); // Convertirlo en un arreglo asociativo si lo deseas
+
+// Imprimir el JSON en formato legible
+try {
+  $db->exec("SET TIME ZONE 'America/Lima'");
+
+  // Validar el intento antes de actualizar
+  $stmt = $db->prepare("SELECT num_report FROM induction_workers WHERE id = :cabecera_id");
+  $stmt->bindParam(':cabecera_id', $cabecera_id);
+  $stmt->execute();
+  $intentos = $stmt->fetch(PDO::FETCH_ASSOC);
+
+  if ($intentos['num_report'] < $nuevoIntento) {
+    // Iniciar una transacción
+    $db->beginTransaction();
+
+    if ($intentoEntrenamiento != 0) {
+      $nuevoIntento = $nuevoIntento - 1;
+    }
+    // echo "nuevo intento" . $nuevoIntento;
+    try {
+      // Actualizar Intento
+      $stmtUpdate = $db->prepare("UPDATE induction_workers SET num_report = :intento, updated_at = CURRENT_TIMESTAMP WHERE id = :cabecera_id");
+      $stmtUpdate->bindParam(':intento', $nuevoIntento);
+      $stmtUpdate->bindParam(':cabecera_id', $cabecera_id);
+      $stmtUpdate->execute();
+
+      // echo "Intento Actalizado";
+      if ($stmtUpdate->rowCount() > 0) {
+        // echo "Bandera Cabecera<br>";
+
+        // La actualización se realizó con éxito
+        // Inicializa una variable para contar los casos insertados
+        $totalCasosInsertados = 0;
+        $error = false;
+
+        // Recorrer el JSON y validar los registros en detail_induction_workers
+        // Verificar si ya existe un registro en detail_induction_workers
+        if ($intentoEntrenamiento > 0) {
+          $nuevoIntento = $intentoEntrenamiento;
+        }
+
+        $stmtCheck = $db->prepare("SELECT COUNT(*) as count FROM detail_induction_workers WHERE induction_worker_id = :cabecera_id AND report = :nuevoIntento");
+        $stmtCheck->bindParam(':cabecera_id', $cabecera_id);
+        $stmtCheck->bindParam(':nuevoIntento', $nuevoIntento);
+        $stmtCheck->execute();
+        $count = $stmtCheck->fetch(PDO::FETCH_ASSOC);
+        // echo "Intento Entrenamiento: " . $nuevoIntento;
+        foreach ($jsonData as $item) {
+
+          if ($count['count'] == 0 or $intentoEntrenamiento != 0) {
+            // No existe un registro, puedes insertarlo
+            $stmtInsert = $db->prepare('INSERT INTO detail_induction_workers (induction_worker_id, "case", identified, risk_level, correct_measure, "time", difficulty, report, note, note_reference, "start_date", end_date, num_errors,"json",rol, created_at, updated_at, entrenamiento) VALUES (:induction_worker_id, :case, :identified, :risk_level, :correct_measure, :time, :difficulty, :report, :note, :note_reference, :start_date, :end_date, :num_errors,:json,:rol, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, :entrenamiento)');
+            $stmtInsert->bindParam(':induction_worker_id', $cabecera_id);
+            $stmtInsert->bindParam(':case', $item['case']);
+            $stmtInsert->bindParam(':identified', $item['identified']);
+            $stmtInsert->bindParam(':risk_level', $item['risk_level']);
+            $stmtInsert->bindParam(':correct_measure', $item['correct_measure']);
+            $stmtInsert->bindParam(':time', $item['time']);
+            $stmtInsert->bindParam(':difficulty', $item['difficulty']);
+            $stmtInsert->bindParam(':json', json_encode($item['json']));
+            $stmtInsert->bindParam(':rol', $rol);
+            $stmtInsert->bindParam(':report', $nuevoIntento);
+            $stmtInsert->bindParam(':note', $note, PDO::PARAM_STR);
+            $stmtInsert->bindParam(':note_reference', $note_reference, PDO::PARAM_STR);
+            $stmtInsert->bindParam(':start_date', $start_date, PDO::PARAM_STR); // Asegúrate de que $start_date sea un string con formato de fecha válido
+            $stmtInsert->bindParam(':end_date', $end_date, PDO::PARAM_STR); // Asegúrate de que $end_date sea un string con formato de fecha válido
+            $stmtInsert->bindParam(':num_errors', $item['num_errors']); // Reemplaza $num_errors con el valor que desees insertar
+            $stmtInsert->bindParam(':entrenamiento', $entrenamiento, PDO::PARAM_STR);
+
+            if ($stmtInsert->execute()) {
+              // echo "Inserción exitosa para el caso: " . $item['case'] . "<br>";
+            } else {
+              //   Se encontró un error en la inserción
+              $error = true;
+              // echo "Error en la inserción para el caso: " . $item['case'] . "<br>";
+              break; // Salir del bucle para evitar más inserciones
+            }
+            $totalCasosInsertados++;
+          }
+        }
+
+        if ($error) {
+          // Si se encuentra un error en la inserción, revertir la transacción
+          $db->rollBack();
+          echo json_encode(['error' => 'Error en la inserción de casos. Se eliminaron todos los registros y no se realizó la actualización.']);
+        } else {
+          // Si todo va bien, confirmar la transacción
+          $db->commit();
+          $mensajeRespuesta = 'Actualización exitosa';
+          if ($totalCasosInsertados > 0) {
+            $mensajeRespuesta .= ". Se insertaron $totalCasosInsertados casos.";
+          }
+          echo json_encode(['success' => $mensajeRespuesta]);
+        }
+      } else {
+        echo json_encode(['error' => 'No se actualizó ningún registro.']);
+      }
+    } catch (PDOException $ex) {
+      // En caso de error en la actualización
+      $db->rollBack();
+      echo json_encode(['error' => 'Error en la actualización: ' . $ex->getMessage()]);
+    }
+  } else {
+    echo json_encode(['error' => 'Ya existe un reporte generado para este intento']);
+  }
+} catch (PDOException $ex) {
+  // Manejar errores PDO
+  echo json_encode(['error' => 'Error en la consulta: ' . $ex->getMessage()]);
+} finally {
+  // Restablecer la zona horaria de la sesión a su valor predeterminado
+  $db->exec("RESET TIME ZONE");
+}
